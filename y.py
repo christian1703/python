8f6291b71c5b9b14216360ec18ad8a12a2766a64
christian1703 <ristiantbg17@gmail.com>
2023-03-02 11:15:30 +0700

    first commit

--- y.py
+++ y.py
@@ -1,6 +1,6 @@
 import logging
 from scapy.all import *
-from datetime import 
+from datetime import dattime
 
 # Set up logging configuration
 logging.basicConfig(filename='logs.log', level=logging.DEBUG)
